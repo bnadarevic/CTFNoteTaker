0580a0cb4436a0d87fa485487c31dc806dd4d223
ValarDragon <dojha12@gmail.com>
2016-08-17 19:28:44 -0500

    Forgot to commit update to conf.py

--- Utilities/conf.py
+++ Utilities/conf.py
@@ -4,9 +4,13 @@ CHAN="#bots"
 NICK = "CTFNoteTaker"
 IDENT = "CTF_"
 REALNAME = "Hal Glados"
-MASTER = ["valar_dragon","NETWORKsecurity,CTFNoteTaker"] #NO HUMAN WILL RULE ME!
+MASTER = ["valar_dragon, NETWORKsecurity, CTFNoteTaker"] #NO HUMAN WILL RULE ME!
 
 BANNEDPHRASEMSG = "You used a banned phrase!"
 ADMINCOMMANDMODE = "MASTER"
 COMMANDPREFIX = "."
 ADMINCOMMANDPREFIX = "~"
+
+ESCAPECHAR = "\\"
+#It is important that we expect ESCAPE<STRING> To be unique.
+ESCAPECOMMA = "|COMMA|"
