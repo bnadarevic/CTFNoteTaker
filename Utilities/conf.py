8a7e680815a9a97cfe03c6ecdf8cc2fca2a26f13
Pyhscript <pythoncoder@hmamail.com>
2016-08-19 01:00:28 +0200

    ensured trailing parentheses is there in new syntax for .add

--- Utilities/conf.py
+++ Utilities/conf.py
@@ -1,7 +1,7 @@
 HOST = "irc.hackthissite.org"
 PORT = 6667
 CHAN="#bots"
-NICK = "CTFNoteTaker"
+NICK = "CTFNoteTaker_test"
 IDENT = "CTF_"
 REALNAME = "Hal Glados"
 MASTER = ["valar_dragon, NETWORKsecurity, CTFNoteTaker"] #NO HUMAN WILL RULE ME!
