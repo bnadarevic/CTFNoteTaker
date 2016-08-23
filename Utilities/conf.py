cb1c17f41ffc4ffedfbfe6028a04a75bcf0cea69
Pyhscript <pythoncoder@hmamail.com>
2016-08-23 02:44:35 +0200

    Update conf.py

--- Utilities/conf.py
+++ Utilities/conf.py
@@ -1,7 +1,7 @@
 HOST = "irc.hackthissite.org"
 PORT = 6667
 CHAN="#bots"
-NICK = "CTFNoteTaker_"
+NICK = "CTFNoteTaker"
 IDENT = "CTF_"
 REALNAME = "Hal Glados"
 MASTER = ["valar_dragon", "NETWORKsecurity", "CTFNoteTaker"] #NO HUMAN WILL RULE ME!
